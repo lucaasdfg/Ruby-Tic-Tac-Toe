f58e814701048711ac0936a0aa36f3d1fcc79359
Lucasdfg <cervantes.lucas99@gmail.com>
2019-11-05 16:42:18 -0600

    added board and users

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 class Game
+  attr_accessor :counter
   puts "your turn..."
   def initialize
     puts "Tell me your name..."
@@ -7,18 +8,14 @@ class Game
     puts "And your opponent name..."
     @player2 = gets.chomp
     @counter = 1
-    puts "put your choice.."
-    @choice = gets.chomp
-    puts board.display_board
-    board.mark_choice(@choice)
-    puts board.display_board
+    
   end
 end
 
 class Board
   def initialize
     @board = {a1: "", a2: "", a3: "", b1: "", b2:"", b3:"", c1: "", c2: "", c3:""}
-    @turn = ""
+    @turn = "X"
   end
 
   def mark_choice(choice)
@@ -28,17 +25,21 @@ class Board
       @turn == 'O'
     end
 
-    @board[choice] = "X" if @turn == 'X'
+    @board[choice] = "X" if @turn == 'X' 
     @board[choice] = "O" if @turn == 'O'
   end
 
   def display_board
-    puts "#{@board}"
+    puts "#{@board[:a1]} | #{@board[:a2]} | #{@board[:a3]} \n | #{@board[:b1]} | #{@board[:b2]} | #{@board[:b3]} \n | #{@board[:c1]} | #{@board[:c2]} | #{@board[:c3]} |"
   end
 end
 
 board = Board.new
 start = Game.new
 start
+
+puts "put your choice.."
+board.mark_choice(gets.chomp.to_sym)
+puts board.display_board
 
 
