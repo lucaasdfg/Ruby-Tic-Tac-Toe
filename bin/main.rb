c73d9a7034d19cb03e639da66fbc5dd998371c6c
Lucasdfg <cervantes.lucas99@gmail.com>
2019-11-06 14:08:48 -0600

    added game

--- bin/main.rb
+++ bin/main.rb
@@ -19,7 +19,7 @@ class Board
   def initialize
     @board = {a1: "", a2: "", a3: "", b1: "", b2:"", b3:"", c1: "", c2: "", c3:""}
     @turn = "X"
-    @result = ''
+    @result = false
   end
 
   def mark_choice(choice, counter)
@@ -38,9 +38,21 @@ class Board
   def check_victory
     puts @board[:a1]
     puts @result
-    if (@board[:a1] == @board[:a2]) && (@board[:a2] == @board[:a3]) && (@board[:a1] == @board[:a3])
-    @result = "victory"
+    arr = [[@board[:a1], @board[:a2], @board[:a3]],[@board[:b1],@board[:b2],@board[:b3]],[@board[:c1],@board[:c2],@board[:c3]],[@board[:a1], @board[:b1], @board[:c1]], [@board[:a2], @board[:b2], @board[:c2]], [@board[:a3], @board[:b3], @board[:c3]], [@board[:a1], @board[:b2], @board[:c3]], [@board[:a3], @board[:b2], @board[:c1]]]
+    arr.each{ |item|
+    puts "#{item[0]}"
+    puts @result
+    if (item[0] == item[1]) && (item[1] == item[2]) && (item[2] == item[0]) && (item[0] != "")
+      @result = true
     end
+    }
+    
+    
+  
+    
+#if (@board[:a1] == @board[:a2]) && (@board[:a2] == @board[:a3]) && (@board[:a1] == @board[:a3])
+#@result = true
+#end
 
   end
 
@@ -56,11 +68,15 @@ board = Board.new
 
 
 
-while board.result != 'victory' || board.result != 'draw'
+while board.result == false
 
 puts "Make your choice..."
 board.mark_choice(gets.chomp, first_game.counter)
+break if board.result == true
 board.display_board
 first_game.counter == 1 ? first_game.counter=0 : first_game.counter=1
 end
-puts board.result
+if board.result == true 
+  puts "victory" 
+  board.display_board
+end
