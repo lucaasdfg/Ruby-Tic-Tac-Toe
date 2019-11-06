b5f29496de6d14a44966d0e8078d0ff08c6533ee
Lucasdfg <cervantes.lucas99@gmail.com>
2019-11-06 16:46:57 -0600

    added a lot of features

--- bin/main.rb
+++ bin/main.rb
@@ -15,14 +15,16 @@ end
 
 class Board
 
-  attr_reader :result
+  attr_reader :result, :contador
   def initialize
     @board = {a1: "", a2: "", a3: "", b1: "", b2:"", b3:"", c1: "", c2: "", c3:""}
     @turn = "X"
     @result = ""
+    @contador = 0
   end
 
   def mark_choice(choice, counter)
+    @contador += 1
     counter == 1 ? @turn = "X" : @turn = "O"
     @board[choice.to_sym] = "X" if @turn == 'X'
     @board[choice.to_sym] = "O" if @turn == 'O'
@@ -37,39 +39,31 @@ class Board
 
     arr = [[@board[:a1], @board[:a2], @board[:a3]],[@board[:b1],@board[:b2],@board[:b3]],[@board[:c1],@board[:c2],@board[:c3]],[@board[:a1], @board[:b1], @board[:c1]], [@board[:a2], @board[:b2], @board[:c2]], [@board[:a3], @board[:b3], @board[:c3]], [@board[:a1], @board[:b2], @board[:c3]], [@board[:a3], @board[:b2], @board[:c1]]]
     arr.each{ |item|
-    puts "#{item[0]}"
-    puts @result
+    
     if (item[0] == item[1]) && (item[1] == item[2]) && (item[2] == item[0]) && (item[0] != "")
       @result = "victory"
     end
     }
-
+    if @contador == 9
+      @result = "draw"
+    end
   end
-  def check_draw
-
-  end
-
   def check_valid_choice?(arg)
-    choice_dict = ["a1", "a2"]
+    choice_dict = ["a1", "a2","a3", "b1", "b2", "b3", "c1", "c2", "c3"]
     
-    if choice_dict.include?(arg)
+    if choice_dict.include?(arg) && @board[arg.to_sym] == ""
       return true
     else
-      puts "make a valid choice"
+      puts "invalid choice"
     end
-
-
   end
-
-
 end
 
 =begin
 
-1) check valid choice, if true, mark choice on "board" if not, ask  for choice
-2) everytime a valid choice is marked on board, increment "some counter" by one
-3) if "some counter" is equal to 9, and there are no winner, declare a "draw"
-
+1) que se vea mejor el board
+2) mejorar las frases
+3)
 
 =end
 
@@ -82,18 +76,25 @@ while board.result != "victory" || board.result != "draw"
 
 
 puts "Mark your choice..."
-
-valid_choice = gets.chomp
-if board.check_valid_choice?(gets.chomp)
-  board.mark_choice(valid_choice, first_game.counter)
+algo = gets.chomp
+if board.check_valid_choice?(algo)
+  board.mark_choice(algo, first_game.counter)
   break if board.result == "victory" || board.result == "draw"
   board.display_board
   first_game.counter == 1 ? first_game.counter=0 : first_game.counter=1 
+
 end
 
+
+end
 if board.result == "victory"
+  board.display_board
+
   puts "victory"
 
-  board.display_board
 end
+if board.result == "draw"
+  board.display_board
+  puts "draw"
+
 end
