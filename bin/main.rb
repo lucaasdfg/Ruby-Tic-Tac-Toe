0d3fd82c6b238a4727a142d7e826c8988e7f0c36
alex <alexawesomecode@gmail.com>
2019-11-08 18:58:16 -0300

    added lib

--- bin/main.rb
+++ bin/main.rb
@@ -2,54 +2,50 @@
 
 class Game
   attr_accessor :counter
-  puts "your turn..."
+  puts 'your turn...'
   def initialize
-    puts "Tell me your name..."
+    puts 'Tell me your name...'
     @player1 = gets.chomp
-    puts "And your opponent name..."
+    puts 'And your opponent name...'
     @player2 = gets.chomp
     @counter = 1
-
   end
 
   def start
-
     result = 'sometext'
     while result != 'victory' || result != 'draw'
 
-    puts "Make your choice..."
-    board.mark_choice(gets.chomp)
-    board.display_board
-    @counter = 0 if @counter != 0
-    @counter = 1 if @counter == 0
+      puts 'Make your choice...'
+      board.mark_choice(gets.chomp)
+      board.display_board
+      @counter = 0 if @counter != 0
+      @counter = 1 if @counter == 0
     end
     puts result
-
   end
 end
 
 class Board
   def initialize
-    @board = {a1: "", a2: "", a3: "", b1: "", b2:"", b3:"", c1: "", c2: "", c3:""}
-    @turn = "X"
+    @board = { a1: '', a2: '', a3: '', b1: '', b2: '', b3: '', c1: '', c2: '', c3: '' }
+    @turn = 'X'
   end
 
   def mark_choice(choice)
-    if @counter == 1
-      @turn == 'X'
-    else
-      @turn == 'O'
-    end
+    @turn == if @counter == 1
+               'X'
+             else
+               'O'
+             end
 
-    @board[choice] = "X" if @turn == 'X' 
-    @board[choice] = "O" if @turn == 'O'
+    @board[choice] = 'X' if @turn == 'X'
+    @board[choice] = 'O' if @turn == 'O'
   end
 
   def display_board
     puts "#{@board[:a1]} | #{@board[:a2]} | #{@board[:a3]} \n | #{@board[:b1]} | #{@board[:b2]} | #{@board[:b3]} \n | #{@board[:c1]} | #{@board[:c2]} | #{@board[:c3]} |"
   end
 end
-
 
 first_game = Game.new
 first_game.start
