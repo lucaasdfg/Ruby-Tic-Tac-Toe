5a411c9d23ed372314968154ca3c5136b3ff1d06
alex <alexawesomecode@gmail.com>
2019-11-11 16:33:34 -0300

    fixed user interface

--- bin/main.rb
+++ bin/main.rb
@@ -1,51 +1,46 @@
 #!/usr/bin/env ruby
 
-class Game
-  attr_accessor :counter
-  puts 'your turn...'
-  def initialize
-    puts 'Tell me your name...'
-    @player1 = gets.chomp
-    puts 'And your opponent name...'
-    @player2 = gets.chomp
-    @counter = 1
-  end
+board = ''
+result = ''
 
-  def start
-    result = 'sometext'
-    while result != 'victory' || result != 'draw'
+# welcome users to game and asks for names
 
-      puts 'Make your choice...'
-      board.mark_choice(gets.chomp)
-      board.display_board
-      @counter = 0 if @counter != 0
-      @counter = 1 if @counter == 0
-    end
-    puts result
-  end
+puts "\nHello. Welcome to Tic Tac Toe Game.\n\n Tell me your name..."
+player_1 = gets.chomp
+puts "\n Okey, #{player_1}. Now tell me your opponent name..."
+player_2 = gets.chomp
+puts "\n Okey, #{player_1} & #{player_2}. {Some explanation on how to play goes here...a}Lets play.... "
+
+# use sleep to create time on user interface changes
+sleep 2.0
+
+#  here display empy board : board
+
+# keep on while if 'result' is different from victory or draw, exits when get some of them
+while result != 'victory' || result != 'draw'
+
+  puts 'Mark your choice...'
+  input_choice = gets.chomp.downcase
+
+  #here use a  'next unless' to check if choice is valid
+  #if valid, mark_choice(input_choice)
+
+  puts board
+
+  #  break if board == 'victory' || board == 'draw'
+
 end
 
-class Board
-  def initialize
-    @board = { a1: '', a2: '', a3: '', b1: '', b2: '', b3: '', c1: '', c2: '', c3: '' }
-    @turn = 'X'
-  end
 
-  def mark_choice(choice)
-    @turn == if @counter == 1
-               'X'
-             else
-               'O'
-             end
+if result == 'victory'
 
-    @board[choice] = 'X' if @turn == 'X'
-    @board[choice] = 'O' if @turn == 'O'
-  end
 
-  def display_board
-    puts "#{@board[:a1]} | #{@board[:a2]} | #{@board[:a3]} \n | #{@board[:b1]} | #{@board[:b2]} | #{@board[:b3]} \n | #{@board[:c1]} | #{@board[:c2]} | #{@board[:c3]} |"
-  end
+  #display board, display winner 
+
+  puts "\n\s\s\s\s!!!! Congrats WINNER-NAME you WON. You are truly a WINNER !!!! \s\s\s\s\n"
+
 end
-
-first_game = Game.new
-first_game.start
+if result == 'draw'
+  #display board, display draw
+  
+end
