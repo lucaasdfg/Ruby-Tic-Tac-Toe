a173395e5e088d11b897e7c64d29faa6aae87dff
alex <alexawesomecode@gmail.com>
2019-11-16 13:03:09 -0300

    fixed spect to fit new classes

---
+++ lib/game.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+class Game
+  attr_writer :board, :contador
+  attr_reader :result, :contador, :winner, :counter, :board, :turn
+
+  def initialize
+    @board = { a1: '', a2: '', a3: '', b1: '', b2: '', b3: '', c1: '', c2: '', c3: '' }
+    @turn = :X
+    @result = ''
+    @contador = 0
+    @winner = ''
+    @counter = 1
+  end
+
+  def mark_choice(choice, _counter)
+    @contador += 1
+    @turn = @counter == 1 ? :X : :O
+    @board[choice.to_sym] = :X if @turn == :X
+    @board[choice.to_sym] = :O if @turn == :O
+    check_victory
+  end
+
+  def display_board
+    "
+    --------------------------
+    \s\s\s\s a|\s #{@board[:a1]}\s|\s #{@board[:a2]}\s |\s #{@board[:a3]}\s |
+    \s\s\s\s b|\s #{@board[:b1]}\s|\s #{@board[:b2]}\s |\s #{@board[:b3]}\s |
+    \s\s\s\s c|\s #{@board[:c1]}\s|\s #{@board[:c2]}\s |\s #{@board[:c3]}\s |
+    --------------------------
+    "
+  end
+
+  def check_victory
+    @result = @result != :VICTORY ? :DRAW : :VICTORY if @contador == 9
+    winning_patterns = [
+      [@board[:a1], @board[:a2], @board[:a3]],
+      [@board[:b1], @board[:b2], @board[:b3]],
+      [@board[:c1], @board[:c2], @board[:c3]],
+      [@board[:a1], @board[:b1], @board[:c1]],
+      [@board[:a2], @board[:b2], @board[:c2]],
+      [@board[:a3], @board[:b3], @board[:c3]],
+      [@board[:a1], @board[:b2], @board[:c3]],
+      [@board[:a3], @board[:b2], @board[:c1]]
+    ]
+
+    winning_patterns.each do |item|
+      @result = :VICTORY if item.all?(:X) || item.all?(:O)
+    end
+  end
+
+  def check_valid_choice?(arg)
+    choice_dict = %w[a1 a2 a3 b1 b2 b3 c1 c2 c3]
+    return true if choice_dict.include?(arg) && @board[arg.to_sym] == ''
+
+    false
+  end
+
+  def check_winner
+    @winner = @turn
+  end
+
+  def check_result
+    return true if @result == :VICTORY || @result == :DRAW
+    return false if @result != :VICTORY || @result != :DRAW
+  end
+
+  def toset_counter
+    @counter = (@counter == 1 ? 0 : 1)
+  end
+  end
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,72 +1,99 @@
-require_relative '../lib/gamelogic.rb'
+require_relative '../lib/game.rb'
+require_relative '../lib/players.rb'
 
+describe Game do
+  let(:first_game) { Game.new }
+  let(:empty_board) { [["", "", ""], ["", "", ""], ["", "", ""], ["", "", ""], ["", "", ""], ["", "", ""], ["", "", ""], ["", "", ""]]
+ }
 
-class Board
-    attr_accessor :counter
-    attr_reader :result, :contador, :winner
-  
-    def initialize
-      @board = { a1: '', a2: '', a3: '', b1: '', b2: '', b3: '', c1: '', c2: '', c3: '' }
-      @turn = 'X'
-      @result = ''
-      @contador = 0
-      @winner = ''
-      @counter = 1
+  context 'test initialize values of object with class first_game' do
+    it 'starts turn with X' do
+      expect(first_game.turn).to eq(:X)
     end
-  
-    def mark_choice(choice, _counter)
-      @contador += 1
-      @turn = @counter == 1 ? 'X' : 'O'
-      @board[choice.to_sym] = 'X' if @turn == 'X'
-      @board[choice.to_sym] = 'O' if @turn == 'O'
-      check_victory
+
+    it 'starts counter with 1' do
+      expect(first_game.counter).to eq(1)
     end
-  
-    def display_board
-      puts "--------------------------\n\s\s\s\s a|\s #{@board[:a1]}\s |\s #{@board[:a2]}\s |\s #{@board[:a3]}\s |\n\s\s\s\s b|\s #{@board[:b1]}\s |\s #{@board[:b2]}\s |\s #{@board[:b3]}\s |\n\s\s\s\s c|\s #{@board[:c1]}\s |\s #{@board[:c2]}\s |\s #{@board[:c3]}\s | \n--------------------------"
+
+    it "starts 'result' with empty strings" do
+      expect(first_game.result).to eq('')
     end
-  
-    def check_victory
-      winning_patterns = [[@board[:a1], @board[:a2], @board[:a3]], [@board[:b1], @board[:b2], @board[:b3]], [@board[:c1], @board[:c2], @board[:c3]], [@board[:a1], @board[:b1], @board[:c1]], [@board[:a2], @board[:b2], @board[:c2]], [@board[:a3], @board[:b3], @board[:c3]], [@board[:a1], @board[:b2], @board[:c3]], [@board[:a3], @board[:b2], @board[:c1]]]
-      winning_patterns.each do |item|
-        next unless (item[0] == item[1]) && (item[1] == item[2]) && (item[2] == item[0]) && (item[0] != '')
-  
-        @result = 'victory'
-      end
-      @result = 'draw' if @contador == 9
-    end
-  
-    def check_valid_choice?(arg)
-      choice_dict = %w[a1 a2 a3 b1 b2 b3 c1 c2 c3]
-  
-      return true if choice_dict.include?(arg) && @board[arg.to_sym] == ''
-  
-      puts "\n\nINVALID CHOICE\n\n You can choose only EMPTY cells using letters & and numbers: a1, a2, b3, c4, etc.\n\n"
-      sleep 2.0
-      display_board
-    end
-  
-    def check_winner
-      @winner = @turn
-    end
-  
-    def check_result
-      return true if @result == 'victory' || @result == 'draw'
-      return false if @result != 'victory' || @result != 'draw'
-    end
-  
-    def toset_counter
-      @counter = (@counter == 1 ? 0 : 1)
+
+    it 'class of created object is equal to Game' do
+      expect(first_game.class).to eq(Game)
     end
   end
 
-  describe Board do 
-    describe "#check_victory" do 
-        it "changes 'result to victory' when it mets one winning pattern"
-        it "when contador = 9  changes result to draw"
-        it "return nothing when is called"
-    end
-    describe "#check_valid_choice?" do 
+  describe '#check_valid_choice?' do
+    it 'should return different to true when no valid choice is given' do
+      expect(first_game.check_valid_choice?('aisdad')).not_to be true
     end
 
+    it 'should return true when valid choice is given' do
+      expect(first_game.check_valid_choice?('a2')).to be true
+    end
   end
+
+  describe '#mark_choice' do
+   
+    it 'raise error when no parameter is given' do
+      expect { first_game.mark_choice }.to raise_error(ArgumentError)
+    end
+
+    it 'add choice to @first_game when there is a valid choice' do
+      first_game.mark_choice('b2', 2)
+      expect(first_game.board[:b2]).to eq(:X)
+    end
+
+    it "should increment 'contador' by one when a valid choice is given" do
+      first_game.mark_choice('b2', first_game.contador)
+      expect(first_game.contador).to eq(1)
+    end
+  end
+
+  describe "#check_victory" do 
+    it "return empty array when cells in board are empty" do
+      expect(first_game.check_victory).to eql(empty_board)
+    end
+    it "when contador = 9  changes result to draw" do
+      first_game.contador = 9
+      first_game.check_victory
+      expect(first_game.result).to eql(:DRAW)
+    end
+    it "changes resut variable to victory when finds a winning patern" do
+      first_game.board = {a1: :X, a2: :X, a3: :X}
+      first_game.check_victory 
+      expect(first_game.result).to eql(:VICTORY)
+      
+    end
+end
+describe "#check_winner" do
+    it "check winner es igual a turn cuando llamas al metodo check winner la variable winner es igual a turn"  do
+     winner = first_game.check_winner
+     expect(winner).to eql(:X)
+  end
+
+end
+end
+
+
+
+
+describe Players do
+let(:players) { Players.new }
+
+describe "#toset_player" do
+    it "cuando llamas a toset player en la variable player el argumento que se pasa" do
+     players.toset_player("lucas")
+     testcase = players.players.include?("lucas") ? true : false
+     expect(testcase).to be true
+end
+end
+
+describe "#initialize" do
+  it  "cuando creas un nuevo objeto con la clase game una variable players que es igual empty array" do
+   expect(players.players).to eql([])
+end
+
+end
+end
