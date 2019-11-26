aee2301ceebef3933b85dd0983b1fd5c8f3b75be
alexawesomecode <alexawesomecode@gmail.com>
2019-11-26 11:17:43 -0300

    fixex rubocop

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,89 +1,87 @@
 require_relative '../lib/game.rb'
 
 describe Game do
-    let(:first_game) { Game.new }
-    let(:empty_board) do
-      [['', '', ''], ['', '', ''], ['', '', ''], ['', '', ''], ['', '', ''], ['', '', ''], ['', '', ''], ['', '', '']]
-    end
-  
-    context 'test initialize values of object with class first_game' do
-      it 'starts turn with X' do
-        expect(first_game.turn).to eq(:X)
-      end
-  
-      it 'starts counter with 1' do
-        expect(first_game.counter).to eq(1)
-      end
-  
-      it "starts 'result' with empty strings" do
-        expect(first_game.result).to eq('')
-      end
-  
-      it 'class of created object is equal to Game' do
-        expect(first_game.class).to eq(Game)
-      end
-    end
-  
-    describe '#check_valid_choice?' do
-      it 'should return different to true when no valid choice is given' do
-        expect(first_game.check_valid_choice?('aisdad')).not_to be true
-      end
-  
-      it 'should return true when valid choice is given' do
-        expect(first_game.check_valid_choice?('a2')).to be true
-      end
+  let(:first_game) { Game.new }
+  let(:empty_board) do
+    [['', '', ''], ['', '', ''], ['', '', ''], ['', '', ''], ['', '', ''], ['', '', ''], ['', '', ''], ['', '', '']]
+  end
 
-      it 'it should keep contador variable without being modified, as it is' do
-        first_game.contador = 3
-        first_game.check_valid_choice?('asada2')
-        expect(first_game.contador).to be 3
-      end
+  context 'test initialize values of object with class first_game' do
+    it 'starts turn with X' do
+      expect(first_game.turn).to eq(:X)
     end
-  
-    describe '#mark_choice' do
-      it 'raise error when no parameter is given' do
-        expect { first_game.mark_choice }.to raise_error(ArgumentError)
-      end
-  
-      it 'add choice to @first_game when there is a valid choice' do
-        first_game.mark_choice('b2', 2)
-        expect(first_game.board[:b2]).to eq(:X)
-      end
-  
-      it "should increment 'contador' by one when a valid choice is given" do
-        first_game.mark_choice('b2', first_game.contador)
-        expect(first_game.contador).to eq(1)
-      end
 
+    it 'starts counter with 1' do
+      expect(first_game.counter).to eq(1)
     end
-  
-    describe '#check_victory' do
-      it 'return empty array when cells in board are empty' do
-        expect(first_game.check_victory).to eql(empty_board)
-      end
-      it 'when contador = 9  changes result to draw' do
-        first_game.contador = 9
-        first_game.check_victory
-        expect(first_game.result).to eql(:DRAW)
-      end
-      it 'changes resut variable to victory when finds a winning patern' do
-        first_game.board = { a1: :X, a2: :X, a3: :X }
-        first_game.check_victory
-        expect(first_game.result).to eql(:VICTORY)
-      end
+
+    it "starts 'result' with empty strings" do
+      expect(first_game.result).to eq('')
     end
-    describe '#check_winner keeps track of @winner equal to turn ' do
-      it 'check winner is eql to :X in first turn' do
-        winner = first_game.turn
-        expect(winner).to eql(:X)
-      end
-      it '#check winner is eql to :O on second turn' do
-        first_game.toset_counter
-        first_game.mark_choice("b1", 0)
-        first_game.check_winner
-        winner = first_game.turn
-        expect(winner).to eql(:O)
-      end
+
+    it 'class of created object is equal to Game' do
+      expect(first_game.class).to eq(Game)
     end
   end
-  
+
+  describe '#check_valid_choice?' do
+    it 'should return different to true when no valid choice is given' do
+      expect(first_game.check_valid_choice?('aisdad')).not_to be true
+    end
+
+    it 'should return true when valid choice is given' do
+      expect(first_game.check_valid_choice?('a2')).to be true
+    end
+
+    it 'it should keep contador variable without being modified, as it is' do
+      first_game.contador = 3
+      first_game.check_valid_choice?('asada2')
+      expect(first_game.contador).to be 3
+    end
+  end
+
+  describe '#mark_choice' do
+    it 'raise error when no parameter is given' do
+      expect { first_game.mark_choice }.to raise_error(ArgumentError)
+    end
+
+    it 'add choice to @first_game when there is a valid choice' do
+      first_game.mark_choice('b2', 2)
+      expect(first_game.board[:b2]).to eq(:X)
+    end
+
+    it "should increment 'contador' by one when a valid choice is given" do
+      first_game.mark_choice('b2', first_game.contador)
+      expect(first_game.contador).to eq(1)
+    end
+  end
+
+  describe '#check_victory' do
+    it 'return empty array when cells in board are empty' do
+      expect(first_game.check_victory).to eql(empty_board)
+    end
+    it 'when contador = 9  changes result to draw' do
+      first_game.contador = 9
+      first_game.check_victory
+      expect(first_game.result).to eql(:DRAW)
+    end
+    it 'changes resut variable to victory when finds a winning patern' do
+      first_game.board = { a1: :X, a2: :X, a3: :X }
+      first_game.check_victory
+      expect(first_game.result).to eql(:VICTORY)
+    end
+  end
+  describe '#check_winner keeps track of @winner equal to turn ' do
+    it 'check winner is eql to :X in first turn' do
+      winner = first_game.turn
+      expect(winner).to eql(:X)
+    end
+    it '#check winner is eql to :O on second turn' do
+      first_game.toset_counter
+      first_game.mark_choice('b1', 0)
+      first_game.check_winner
+      winner = first_game.turn
+      expect(winner).to eql(:O)
+    end
+  end
+end
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -2,9 +2,9 @@
 
 require_relative '../bin/main.rb'
 
- describe "#display_invalid_choice" do
-   it "should display text when called" do
-     test = "\n\nINVALID CHOICE\n\n You can choose only EMPTY cells using letters & numbers: a1, a2, b3, c4, etc.\n\n"
+describe '#display_invalid_choice' do
+  it 'should display text when called' do
+    test = "\n\nINVALID CHOICE\n\n You can choose only EMPTY cells using letters & numbers: a1, a2, b3, c4, etc.\n\n"
     expect(display_invalid_choice).to eql(test)
-    end
- end
+  end
+end
